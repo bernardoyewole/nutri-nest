3ecab29e5ab69318fb9e7e3f10c71505e183ad6d
Bernard <oyebenard@gmail.com>
2025-02-03 23:18:19 -0600

    Add pan gesture swipe in onboarding steps

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -1,11 +1,13 @@
 import {View, Text, Image, TouchableOpacity, Dimensions} from 'react-native';
-import { useState } from 'react';
+import {useRef, useState} from 'react';
 import { useDispatch } from 'react-redux';
 import { completeOnboarding } from '@/redux/slices/onboardingSlice';
-import { useNavigation } from '@react-navigation/native';
 import {LinearGradient} from "expo-linear-gradient";
 import {useRouter} from "expo-router";
 import "../global.css";
+import Animated, {useAnimatedScrollHandler, useSharedValue} from "react-native-reanimated";
+
+const { width, height } = Dimensions.get("window");
 
 const onboardingSteps = [
     {
@@ -28,66 +30,89 @@ const onboardingSteps = [
 export default function OnboardingScreen() {
     const [step, setStep] = useState(0);
     const dispatch = useDispatch();
-    const navigation = useNavigation();
     const router = useRouter();
-    const width = Dimensions.get("window").width;
-    const height = Dimensions.get("window").height;
+    const scrollX = useSharedValue(0);
+    const scrollRef = useRef(null);
 
     const nextStep = () => {
         if (step < onboardingSteps.length - 1) {
-            setStep(step + 1);
+            const newStep = step + 1;
+            setStep(newStep);
+            scrollRef.current?.scrollTo({ x: newStep * width, animated: true });
         } else {
             dispatch(completeOnboarding());
-            router.replace("/(auth)/signup");
+            router.navigate("/(auth)/signup");
         }
     };
 
+    const scrollHandler = useAnimatedScrollHandler({
+        onScroll: (event) => {
+            scrollX.value = event.contentOffset.x;
+        }
+    });
+
     return (
-        <View className="flex-1">
-            {/* Background Image */}
-            <Image
-                source={onboardingSteps[step].image}
-                style={{ position: "absolute", width: width, height: height }}
-                resizeMode="cover"
-            />
+        <Animated.ScrollView
+            ref={scrollRef}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            onScroll={scrollHandler}
+            scrollEventThrottle={16}
+            className='flex-1'
+        >
+            {onboardingSteps.map((stepData, index) => (
+                <View key={index} style={{ width }}>
+                    {/* Background Image */}
+                    <Image
+                        source={stepData.image}
+                        style={{ position: "absolute", width, height }}
+                        resizeMode="cover"
+                    />
 
-            {/* Gradient Overlay */}
-            <LinearGradient
-                style={{ position: "absolute", width: width, height: height }}
-                colors={['transparent', "#dcfce7"]}
-            />
+                    {/* Gradient Overlay */}
+                    <LinearGradient
+                        style={{ position: "absolute", width, height }}
+                        colors={['transparent', "#dcfce7"]}
+                    />
 
-            {/* Wrapper for Onboarding Content */}
-            <View style={{ flex: 1, justifyContent: "flex-end" }}>
-                <View className="bg-green-100 p-6 rounded-t-3xl shadow-lg w-full">
-                    <Text className="text-3xl font-bold text-gray-800 text-center">{onboardingSteps[step].title}</Text>
-                    <Text className="text-center text-gray-600 my-10 text-lg">{onboardingSteps[step].description}</Text>
-                    <View className="flex-row justify-center">
-                        {onboardingSteps.map((_, index) => (
-                            <TouchableOpacity key={index} onPress={() => setStep(index)}>
-                                <View
-                                    className={`w-8 h-2 mx-1 rounded-full ${step === index ? 'bg-green-700' : 'bg-yellow-300'}`}
-                                />
+                    {/* Wrapper for Onboarding Content */}
+                    <View style={{ flex: 1, justifyContent: "flex-end" }}>
+                        <View className="bg-green-100 p-6 rounded-t-3xl shadow-lg w-full">
+                            <Text className="text-3xl font-bold text-gray-800 text-center">
+                                {stepData.title}
+                            </Text>
+                            <Text className="text-center text-gray-600 my-10 text-lg">
+                                {stepData.description}
+                            </Text>
+
+                            <View className="flex-row justify-center">
+                                {onboardingSteps.map((_, indexTwo) => (
+                                    <View
+                                        key={indexTwo}
+                                        className={`w-8 h-2 mx-1 rounded-full ${index === indexTwo ? 'bg-green-700' : 'bg-yellow-300'}`}
+                                    />
+                                ))}
+                            </View>
+
+                            <TouchableOpacity
+                                onPress={() => router.navigate("/(auth)/signup")}
+                                style={{ alignSelf: "flex-end", paddingVertical: 24 }}
+                            >
+                                <Text style={{ color: "green", textDecorationLine: "underline", fontWeight: "bold", fontSize: 18 }}>
+                                    SKIP
+                                </Text>
                             </TouchableOpacity>
-                        ))}
+
+                            <TouchableOpacity onPress={nextStep} className="bg-red-500 py-4 mb-6 rounded-lg">
+                                <Text className="text-white font-semibold text-center text-lg">
+                                    {index === onboardingSteps.length - 1 ? "GET STARTED" : "NEXT"}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                    <TouchableOpacity
-                        onPress={() => router.replace("/(auth)/signup")}
-                        style={{ alignSelf: "flex-end", paddingVertical: 24 }}
-                    >
-                        <Text style={{ color: "green", textDecorationLine: "underline", fontWeight: "bold", fontSize: 18 }}>
-                            SKIP
-                        </Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={nextStep} className="bg-red-500 py-4 mb-6 rounded-lg">
-                        <Text className="text-white font-semibold text-center text-lg">
-                            {step === onboardingSteps.length - 1 ? "GET STARTED" : "NEXT"}
-                        </Text>
-                    </TouchableOpacity>
                 </View>
-            </View>
-        </View>
-
+            ))}
+        </Animated.ScrollView>
     );
 }
